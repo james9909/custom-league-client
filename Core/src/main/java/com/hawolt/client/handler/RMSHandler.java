76245522d104e3459dbdf2d57883f96b51551db9
hawolt <45050975+hawolt@users.noreply.github.com>
2023-08-20 13:53:40 +0200

    update rmshandler according to rms library update

--- Core/src/main/java/com/hawolt/client/handler/RMSHandler.java
+++ Core/src/main/java/com/hawolt/client/handler/RMSHandler.java
@@ -5,22 +5,10 @@ import com.hawolt.logger.Logger;
 import com.hawolt.rms.IRiotMessageServiceConnectionCallback;
 import com.hawolt.rms.VirtualRiotMessageClient;
 import com.hawolt.rms.data.GenericRiotMessageEvent;
-import com.hawolt.version.local.LocalRiotFileVersion;
-import com.hawolt.virtual.clientconfig.impl.PlayerClientConfig;
-import com.hawolt.virtual.clientconfig.impl.rms.RMSAffinity;
-import com.hawolt.virtual.clientconfig.impl.rms.RiotMessageServiceConfig;
-import com.hawolt.virtual.leagueclient.authentication.RMS;
-import com.hawolt.virtual.leagueclient.client.Authentication;
 import org.java_websocket.handshake.ServerHandshake;
-import org.json.JSONObject;
 
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * Created: 11/08/2023 18:22
@@ -30,31 +18,14 @@ import java.util.UUID;
 public class RMSHandler implements IRiotMessageServiceConnectionCallback {
     private final VirtualRiotMessageClient virtualRiotMessageClient;
     private final LeagueClient client;
-    private final URI url;
 
     public static RMSHandler build(LeagueClient client) throws URISyntaxException, MalformedURLException {
-        RMS rms = (RMS) client.getVirtualLeagueClient().get(Authentication.RMS);
-        String affinity = rms.get("rms_token");
-        JSONObject content = new JSONObject(new String(Base64.getDecoder().decode(affinity.split("\\.")[1])));
-        PlayerClientConfig playerClientConfig = client.getVirtualLeagueClientInstance().getPlayerClientConfig();
-        RiotMessageServiceConfig riotMessageServiceConfig = playerClientConfig.getRiotMessageServiceConfig();
-        String url = String.format(
-                "%s:443/rms/v1/session?token=%s&id=%s&token_type=access&product_id=league_of_legends&platform=windows&device=desktop",
-                riotMessageServiceConfig.getRiotMessageServiceAffinity(RMSAffinity.AFFINITIES, content.getString("affinity")),
-                client.getVirtualLeagueClient().getVirtualLeagueClientInstance().getLeagueClientSupplier().get("lol.access_token", true),
-                UUID.randomUUID()
-        );
-        Map<String, String> headers = new HashMap<>();
-        LocalRiotFileVersion versionSupplier = client.getVirtualRiotClientInstance().getLocalRiotFileVersion();
-        headers.put("User-Agent", String.format("RiotClient/%s entitlements (;;;)", versionSupplier.getVersionValue("RiotGamesApi.dll")));
-        headers.put("X-Riot-Affinity", affinity);
-        return new RMSHandler(client, headers, new URI(url));
+        return new RMSHandler(client);
     }
 
-    public RMSHandler(LeagueClient client, Map<String, String> headers, URI url) {
-        this.virtualRiotMessageClient = new VirtualRiotMessageClient(url, headers, this);
+    public RMSHandler(LeagueClient client) {
         this.client = client;
-        this.url = url;
+        this.virtualRiotMessageClient = VirtualRiotMessageClient.create(client.getVirtualLeagueClient(), this);
     }
 
     public RMSHandler connect() {
@@ -64,10 +35,6 @@ public class RMSHandler implements IRiotMessageServiceConnectionCallback {
 
     public VirtualRiotMessageClient getVirtualRiotMessageClient() {
         return virtualRiotMessageClient;
-    }
-
-    public URI getUrl() {
-        return url;
     }
 
     @Override
