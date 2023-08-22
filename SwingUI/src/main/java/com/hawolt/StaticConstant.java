96a43324cdf7816ecc2d3b6baaf299aec4e1b8aa
hawolt <45050975+hawolt@users.noreply.github.com>
2023-08-22 08:37:53 +0200

    add cache path

--- SwingUI/src/main/java/com/hawolt/StaticConstant.java
+++ SwingUI/src/main/java/com/hawolt/StaticConstant.java
@@ -1,10 +1,14 @@
 package com.hawolt;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created: 16/08/2023 18:34
  * Author: Twitter @hawolt
  **/
 
 public class StaticConstant {
+    public static final Path APPLICATION_CACHE = Paths.get(System.getProperty("java.io.tmpdir")).resolve("swift-rift");
     public static final String USER_AGENT = "Swift-Rift";
 }
---
+++ SwingUI/src/main/java/com/hawolt/ui/chat/window/ChatUI.java
@@ -0,0 +1,118 @@
+package com.hawolt.ui.chat.window;
+
+import com.hawolt.ui.chat.friendlist.IFriendListComponent;
+import com.hawolt.util.AudioEngine;
+import com.hawolt.util.panel.ChildUIComponent;
+import com.hawolt.xmpp.core.VirtualRiotXMPPClient;
+import com.hawolt.xmpp.event.handler.message.IMessageListener;
+import com.hawolt.xmpp.event.objects.conversation.history.impl.FailedMessage;
+import com.hawolt.xmpp.event.objects.conversation.history.impl.IncomingMessage;
+import com.hawolt.xmpp.event.objects.conversation.history.impl.OutgoingMessage;
+import com.hawolt.xmpp.event.objects.friends.GenericFriend;
+import com.hawolt.xmpp.event.objects.friends.impl.OnlineFriend;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created: 08/08/2023 20:48
+ * Author: Twitter @hawolt
+ **/
+
+public class ChatUI extends ChildUIComponent implements IMessageListener, IChatWindow {
+    private final Map<String, ChatWindowContent> map = new HashMap<>();
+
+    private final CardLayout layout = new CardLayout();
+    private final ChildUIComponent container;
+    private final ChatWindowHeader header;
+    private VirtualRiotXMPPClient xmppClient;
+    private IFriendListComponent component;
+    private String lastOpenedChat;
+
+    public ChatUI() {
+        super(new BorderLayout());
+        this.add(header = new ChatWindowHeader(new BorderLayout()), BorderLayout.NORTH);
+        this.add(container = new ChildUIComponent(this.layout), BorderLayout.CENTER);
+    }
+
+    public void setSupplier(VirtualRiotXMPPClient xmppClient) {
+        this.xmppClient = xmppClient;
+    }
+
+    public ChatWindowHeader getHeader() {
+        return header;
+    }
+
+    private void addChat(String jid) {
+        ChatWindowContent content = new ChatWindowContent(jid, xmppClient, new BorderLayout());
+        this.container.add(jid, content);
+        this.map.put(jid, content);
+        this.revalidate();
+    }
+
+    private boolean isChatConfigured(String jid) {
+        return this.map.containsKey(jid);
+    }
+
+    public ChatWindowContent getChatContainer(String jid) {
+        if (!isChatConfigured(jid)) addChat(jid);
+        return this.map.get(jid);
+    }
+
+    @Override
+    public VirtualRiotXMPPClient getXMPPClient() {
+        return xmppClient;
+    }
+
+    @Override
+    public void showChat(GenericFriend friend) {
+        this.lastOpenedChat = friend.getJID();
+        if (component != null) component.onOpen(friend.getJID());
+        if (!isVisible()) setVisible(true);
+        this.getHeader().setTarget(
+                friend instanceof OnlineFriend ?
+                        ((OnlineFriend) friend).getLOLName() :
+                        String.join(
+                                "#",
+                                friend.getName().toString(),
+                                friend.getTagline().toString()
+                        )
+        );
+        if (!isChatConfigured(lastOpenedChat)) addChat(lastOpenedChat);
+        this.layout.show(container, lastOpenedChat);
+        AudioEngine.play("join_chat.wav");
+    }
+
+    @Override
+    public void setIFriendListComponent(IFriendListComponent component) {
+        this.component = component;
+    }
+
+    @Override
+    public boolean isChatOpened(String jid) {
+        return isVisible() && jid.equals(lastOpenedChat);
+    }
+
+    @Override
+    public void onMessageReceived(IncomingMessage incomingMessage) {
+        if (component != null) component.onMessage(incomingMessage.getFrom());
+        ChatWindowContent content = getChatContainer(incomingMessage.getFrom());
+        content.addMessage(ChatPerspective.OTHER, incomingMessage.getBody());
+        boolean isChatOpen = isChatOpened(incomingMessage.getFrom());
+        AudioEngine.play(isChatOpen ? "standard_msg_receive.wav" : "pm_receive.wav");
+        if (xmppClient != null) {
+            xmppClient.markChatAsRead(incomingMessage.getFrom());
+        }
+    }
+
+    @Override
+    public void onMessageSent(OutgoingMessage outgoingMessage) {
+
+    }
+
+    @Override
+    public void onFailedMessage(FailedMessage failedMessage) {
+
+    }
+}
