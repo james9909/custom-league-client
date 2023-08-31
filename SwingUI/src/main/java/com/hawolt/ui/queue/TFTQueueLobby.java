a336f925c4db82a92c909fae04811be5796959f6
xXTheSebXx <sebastianjerke@gmail.com>
2023-08-31 10:26:54 +0200

    now show summoners in tft lobby (#73)
    fixes #72

--- SwingUI/src/main/java/com/hawolt/ui/queue/TFTQueueLobby.java
+++ SwingUI/src/main/java/com/hawolt/ui/queue/TFTQueueLobby.java
@@ -1,31 +1,95 @@
 package com.hawolt.ui.queue;
 
 import com.hawolt.LeagueClientUI;
+import com.hawolt.async.ExecutorManager;
 import com.hawolt.client.resources.Loadout;
 import com.hawolt.client.resources.ledge.LedgeEndpoint;
 import com.hawolt.client.resources.ledge.parties.PartiesLedge;
-import com.hawolt.client.resources.ledge.parties.objects.PartiesRegistration;
-import com.hawolt.client.resources.ledge.parties.objects.PartyException;
+import com.hawolt.client.resources.ledge.parties.objects.*;
 import com.hawolt.client.resources.ledge.parties.objects.data.PartyAction;
 import com.hawolt.client.resources.ledge.parties.objects.data.TFTLegend;
 import com.hawolt.client.resources.ledge.summoner.SummonerLedge;
 import com.hawolt.client.resources.ledge.summoner.objects.Summoner;
 import com.hawolt.logger.Logger;
+import com.hawolt.rms.data.subject.service.IServiceMessageListener;
+import com.hawolt.rms.data.subject.service.MessageService;
+import com.hawolt.rms.data.subject.service.RiotMessageServiceMessage;
 import com.hawolt.util.panel.ChildUIComponent;
+import org.json.JSONObject;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 /**
  * Created: 11/08/2023 23:00
  * Author: Twitter @hawolt
  **/
 
-public class TFTQueueLobby extends ChildUIComponent {
+public class TFTQueueLobby extends ChildUIComponent implements IServiceMessageListener<RiotMessageServiceMessage> {
+
+    private final LeagueClientUI leagueClientUI;
+    private final ChildUIComponent grid = new ChildUIComponent(new GridLayout(2,4));
+    private ScheduledFuture<?> future;
+
+    private final ScheduledExecutorService scheduler = ExecutorManager.getScheduledService("queue-resumer");
+
+    @Override
+    public void onMessage(RiotMessageServiceMessage riotMessageServiceMessage) {
+        JSONObject payload = riotMessageServiceMessage.getPayload().getPayload();
+        PartiesRegistration registration = new PartiesRegistration(payload.getJSONObject("player"));
+        String puuid = registration.getPUUID();
+        CurrentParty party = registration.getCurrentParty();
+        if (party == null) return;
+        Logger.error(party);
+        List<PartyParticipant> list = party.getPlayers();
+        list.stream().filter(participant -> participant.getPUUID().equals(puuid)).findFirst().ifPresent(self -> {
+            SummonerLedge summonerLedge = leagueClientUI.getLeagueClient().getLedge().getSummoner();
+            try {
+                getSummonerComponentAt(0).update(self, summonerLedge.resolveSummonerByPUUD(puuid));
+                list.remove(self);
+                int memberPosition = 1;
+                for (PartyParticipant participant : list) {
+                    Summoner summoner = summonerLedge.resolveSummonerByPUUD(participant.getPUUID());
+                    if (participant.getRole().equals("MEMBER") || participant.getRole().equals("LEADER")) {
+                        getSummonerComponentAt(memberPosition++).update(participant, summoner);
+                    }
+                }
+                for (int i = memberPosition; i < 5; i++) {
+                    getSummonerComponentAt(i).update(null, null);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            revalidate();
+        });
+    }
+
+    private TFTSummonerComponent getSummonerComponentAt(int id) {
+        int index;
+        if (id == 0) index = 2;
+        else if (id == 1) index = 1;
+        else if (id == 2) index = 3;
+        else if (id == 3) index = 0;
+        else index = 4;
+        return (TFTSummonerComponent) grid.getComponent(index);
+    }
+
+
+
+
     public TFTQueueLobby(LeagueClientUI leagueClientUI, Container parent, CardLayout layout) {
         super(new BorderLayout());
+        this.leagueClientUI = leagueClientUI;
+        this.leagueClientUI.getLeagueClient().getRMSClient().getHandler().addMessageServiceListener(MessageService.PARTIES, this);
         Loadout loadout = new Loadout(leagueClientUI.getLeagueClient());
         JButton close = new JButton("Return to previous Component");
         close.addActionListener(listener -> layout.show(parent, "modes"));
@@ -105,6 +169,13 @@ public class TFTQueueLobby extends ChildUIComponent {
         }
         roles.add(main);
         component.add(roles, BorderLayout.SOUTH);
+
+
+        for (int i = 0; i < 8; i++) grid.add(new TFTSummonerComponent());
+        grid.setBackground(Color.YELLOW);
+        component.add(grid, BorderLayout.CENTER);
+
+
         add(component, BorderLayout.CENTER);
         ChildUIComponent bottom = new ChildUIComponent(new GridLayout(0, 1, 0, 0));
         JButton start = new JButton("START QUEUE");
@@ -115,6 +186,34 @@ public class TFTQueueLobby extends ChildUIComponent {
             try {
                 if (registration == null) partiesLedge.register();
                 partiesLedge.setQueueAction(PartyAction.START);
+                partiesLedge.ready();
+                JSONObject response = partiesLedge.setQueueAction(PartyAction.START);
+                List<GatekeeperRestriction> direct = response.has("errorCode") &&
+                        "GATEKEEPER_RESTRICTED".equals(response.getString("errorCode")) ?
+                        new PartyGatekeeper(response).getRestrictionList() : new ArrayList<>();
+                CurrentParty party = partiesLedge.getOwnPlayer().getCurrentParty();
+                PartyRestriction restriction = party.getPartyRestriction();
+                List<GatekeeperRestriction> indirect = restriction != null ?
+                        restriction.getRestrictionList() : new ArrayList<>();
+                if (direct.isEmpty() && indirect.isEmpty()) return;
+                List<GatekeeperRestriction> sorted = Stream.of(direct, indirect)
+                        .flatMap(Collection::stream)
+                        .sorted(((o1, o2) -> Long.compare(o2.getRemainingMillis(), o1.getRemainingMillis())))
+                        .toList();
+                GatekeeperRestriction gatekeeperRestriction = sorted.get(0);
+                Logger.debug("Restriction: {}", gatekeeperRestriction);
+                leagueClientUI.getChatSidebar().getEssentials().toggleQueueState(
+                        System.currentTimeMillis(),
+                        gatekeeperRestriction.getRemainingMillis(),
+                        true
+                );
+                future = scheduler.schedule(() -> {
+                    try {
+                        partiesLedge.resume();
+                    } catch (IOException e) {
+                        Logger.error(e);
+                    }
+                }, gatekeeperRestriction.getRemainingMillis(), TimeUnit.MILLISECONDS);
             } catch (IOException e) {
                 Logger.error(e);
             }
@@ -122,16 +221,18 @@ public class TFTQueueLobby extends ChildUIComponent {
         bottom.add(start);
         JButton stop = new JButton("STOP QUEUE");
         stop.addActionListener(listener -> {
+            if (future != null) future.cancel(true);
             PartiesLedge partiesLedge = leagueClientUI.getLeagueClient().getLedge().getParties();
             PartiesRegistration registration = partiesLedge.getCurrentRegistration();
             try {
                 if (registration == null) return;
                 partiesLedge.setQueueAction(PartyAction.STOP);
+                leagueClientUI.getChatSidebar().getEssentials().disableQueueState();
             } catch (IOException e) {
                 Logger.error(e);
             }
         });
-        bottom.add(stop);
-        add(bottom, BorderLayout.SOUTH);
+            bottom.add(stop);
+            add(bottom, BorderLayout.SOUTH);
     }
 }
