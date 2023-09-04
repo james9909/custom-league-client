33cab6ef242edeb74e0ca1b49726e55e6bba132b
hawolt <45050975+hawolt@users.noreply.github.com>
2023-09-04 17:26:31 +0200

    prepare for release 1.06 (#100)
    * prepare for release 1.06

---
+++ SwingUI/src/main/java/com/hawolt/ui/champselect/impl/custom/CustomChampSelectUI.java
@@ -0,0 +1,54 @@
+package com.hawolt.ui.champselect.impl.custom;
+
+import com.hawolt.client.resources.communitydragon.spell.Spell;
+import com.hawolt.ui.champselect.AbstractRenderInstance;
+import com.hawolt.ui.champselect.data.ChampSelectType;
+import com.hawolt.ui.champselect.generic.ChampSelectRuneSelection;
+import com.hawolt.xmpp.event.objects.conversation.history.impl.IncomingMessage;
+
+/**
+ * Created: 03/09/2023 14:16
+ * Author: Twitter @hawolt
+ **/
+
+public class CustomChampSelectUI extends AbstractRenderInstance {
+    @Override
+    protected void push(IncomingMessage incomingMessage) {
+
+    }
+
+    @Override
+    protected void stopChampSelectTimer() {
+
+    }
+
+    @Override
+    public void invokeChampionFilter(String champion) {
+
+    }
+
+    @Override
+    public void setGlobalRunePanel(ChampSelectRuneSelection selection) {
+
+    }
+
+    @Override
+    public int[] getSupportedQueueIds() {
+        return new int[0];
+    }
+
+    @Override
+    public String getCardName() {
+        return "custom";
+    }
+
+    @Override
+    public void onSummonerSubmission(Spell selectedSpellOne, Spell selectedSpellTwo) {
+
+    }
+
+    @Override
+    public void onChoiceSubmission(ChampSelectType type, int championId, boolean completed) {
+
+    }
+}
